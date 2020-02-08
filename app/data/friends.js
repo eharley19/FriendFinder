5dc98f70e8ba3a4d9d923eebb0c5aebff7ea5a8d
Edward Harley <edharley19@gmail.com>
2020-02-08 18:22:18 -0500

    Feat: Initial set up ui (html) files

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friends = [
+    {name: "Jane Smith", photo: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", scores: [5, 4, 2, 1, 3, 3, 4, 1, 2, 5] },
+    {name: "Linda Mitchell", photo: "https://www.gettyimages.com/detail/photo/beautiful-woman-wearing-golden-dress-royalty-free-image/666631898", scores: [2, 1, 5, 4, 3, 2, 1, 5, 4, 3] },
+    {name: "Bob Copper", photo: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", scores: [3, 2, 1, 5, 4, 3, 2, 1, 2, 3] },
+    {name: "Errol Perkins", photo: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", scores: [1, 5, 1, 5, 1, 5, 1, 5, 1, 5] },
+    {name: "Greg Givens", photo: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4] },
+    {name: "Hamper Lee", photo: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", scores: [2, 3, 2, 3, 3, 2, 3, 2, 3, 2] }
+
+];
+
+module.exports = friends;
