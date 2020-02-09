ba83fc4c6bfff20341f384a8d040989550e3158e
Edward Harley <edharley19@gmail.com>
2020-02-09 18:40:31 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+
+
+const express = require("express");
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes");
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
