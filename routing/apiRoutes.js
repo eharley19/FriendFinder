d04d73e18a4b6458cb7342718d6fa4d47565e28e
Edward Harley <edharley19@gmail.com>
2020-02-13 03:28:56 -0500

    Feat: Added button and modal to html. Updated post in routing

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,8 +7,13 @@ module.exports = (app) => {
     });
 
     app.post('/api/friends', (req, res) => {
-        
-
+        console.log(req.body);
+        var bestMatch = {};
+        var userScores = req.body.scores;
+        //loop through friends array and compare userScores with scores of each friends object.
+        //calculate difference; determine which friend has the lowest difference.
+        //push user data to friends array.
+        //display bestMatch in modal on html
 
     });
 }
